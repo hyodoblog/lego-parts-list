384ea5cf83a9f8a455cfde8d5a73b9023df92cf8
uuronn <uuronprogram@gmail.com>
2023-04-25 19:25:06 +0900

    queryを使って複数のページのデータを取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,54 @@
 import Image from "next/image";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-type Lego = {
-  partImg: string;
-  partNum: string;
+type PartsItem = {
+  part_num: string;
+  name: string;
+  part_cat_id: number;
+  part_url: string;
+  part_img_url: null;
+  external_ids: { BrickLink: string[]; BrickOwl: string[] };
+  print_of: null;
 };
 
 export default function Home() {
-  const [legoData, setLegoData] = useState<Lego[]>();
+  const [legoData, setLegoData] = useState<PartsItem[]>();
 
   useEffect(() => {
     (async () => {
-      const res = await fetch("/api/hello");
-      const resTest = await fetch("/api/test");
-      const legoData = await res.json();
-      const resData = await resTest.json();
-      console.log("resData", resData);
-      setLegoData(legoData);
+      const testRes = await fetch("/api/test");
+      const testData = await testRes.json();
+      setLegoData(testData);
     })();
   }, []);
 
-  if (!legoData) return <Fragment />;
-
-  // console.log("test", legoData);
+  if (!legoData) return <div>ローディング中です。。。。</div>;
 
   return (
     <main>
-      test
-      <ul className="flex flex-wrap">
-        {legoData.map(
-          (lego, i) =>
-            lego.partImg && (
-              <li className="list-none" key={i}>
-                <p>{lego.partNum}</p>
-                <Image
-                  src={lego.partImg}
-                  width={200}
-                  height={200}
-                  alt={`${lego.partImg}の画像`}
-                  priority
-                />
-              </li>
-            )
-        )}
-      </ul>
+      <div className="p-8">
+        <ul className="grid justify-center grid-cols-8 gap-3">
+          {legoData.map(
+            (lego, i) =>
+              lego.part_img_url && (
+                <li
+                  className="p-4 max-w-xs list-none rounded border-2 border-gray-300 text-center flex flex-col justify-center"
+                  key={i}
+                >
+                  <p>{lego.part_num}</p>
+                  <Image
+                    className="block m-auto"
+                    src={lego.part_img_url}
+                    width={200}
+                    height={200}
+                    alt={`${lego.part_img_url}の画像`}
+                    priority
+                  />
+                </li>
+              )
+          )}
+        </ul>
+      </div>
     </main>
   );
 }
